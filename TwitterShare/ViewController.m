e0647bae18fb2748d9ca7486337c799296f3a57b
Erica <erica@ericas-mac-mini.local>
2017-06-20 16:11:10 -0400

    Obj-C code Twitter like app

--- TwitterShare/ViewController.m
+++ TwitterShare/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
+-(void) configureTweetTextView;
 
 @end
 
@@ -16,7 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self configureTweetTextView];
 }
 
 
@@ -25,5 +29,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showShareAction:(id)sender {
+    if([self.tweetTextView isFirstResponder]){ [self.tweetTextView resignFirstResponder];}
+    UIAlertController *actionController = [UIAlertController alertControllerWithTitle: @"Message Title" message:@"Tweet your message" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle: @"Cancel" style: UIAlertActionStyleDefault handler:nil];
+    
+    UIAlertAction *tweetAction = [UIAlertAction actionWithTitle: @"Tweet" style: UIAlertActionStyleDefault handler: ^(UIAlertAction *action){
+        if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
+            //tweet
+        } else {
+            //Do this
+        }
+        
+    }];
+    
+      [actionController addAction: tweetAction];
+    
+    [actionController addAction: cancelAction];
+    
+    
+    [self presentViewController:actionController animated:YES completion:nil];
+}
+
+-(void) configureTweetTextView {
+    self.tweetTextView.layer.backgroundColor = [UIColor colorWithRed: 1.0 green: 1.0 blue:0.9 alpha: 1.0].CGColor;
+    self.tweetTextView.layer.cornerRadius = 10.0;
+    self.tweetTextView.layer.borderColor = [UIColor colorWithWhite: 0 alpha:0.5].CGColor;
+    self.tweetTextView.layer.borderWidth = 2.0;
+}
 
 @end
